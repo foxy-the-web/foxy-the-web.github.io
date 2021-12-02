54823ae5ba8e871fce5cb931f50c402109b1c04c
Schulz, Rene <Rene.Schulz@otto.de>
2021-12-02 22:48:32 +0100

    Second session on fk-architekt

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
-console.log("Test 2, 6");
+// get dom elements
+const disclaimer = document.getElementById("disclaimer");
+const mainArea = document.getElementById("mainArea");
+
+// get html files
+const loadDoc = (file) => {
+
+  const xhttp = new XMLHttpRequest();
+  
+  xhttp.onreadystatechange = () => {
+
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+    }
+
+  };
+
+  xhttp.open("GET", file);
+  xhttp.send();
+};
+
+disclaimer.onclick = () => {
+  loadDoc('impressum.html');
+}
+
