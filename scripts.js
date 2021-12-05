3f9a989b6c88c68638f06309ae266389e734ad98
Schulz, Rene <Rene.Schulz@otto.de>
2021-12-05 23:43:08 +0100

    Switched to html components and added fetch

--- scripts.js
+++ scripts.js
@@ -1,25 +1,75 @@
-// get dom elements
-const disclaimer = document.getElementById("disclaimer");
-const mainArea = document.getElementById("mainArea");
+// define base component path
+const base = './HTMLcomponents/';
 
-// get html files
-const loadDoc = (file) => {
+// define relevant page elements
+const elements = [
+  'home',
+  'profile',
+  'service',
+  'contact',
+  'disclaimer',
+  'privacyPolicy'
+];
 
-  const xhttp = new XMLHttpRequest();
+// empty nodes object
+let html = {};
+
+// load html document
+const loadHtml = async (file) => {
+  const res = await fetch(file);
   
-  xhttp.onreadystatechange = () => {
-
-    if (this.readyState == 4 && this.status == 200) {
-      console.log(this.responseText);
-    }
-
-  };
-
-  xhttp.open("GET", file);
-  xhttp.send();
-};
-
-disclaimer.onclick = () => {
-  loadDoc('impressum.html');
+  if (res.ok) {
+    const htmlValue = await res.text();
+    return Promise.resolve(htmlValue);
+  } 
+  else {
+    return Promise.reject('*** HTML file not found');
+  } 
 }
 
+const intialize = async () => {
+
+  // get all html files and its content
+  for (element of elements) {
+    html[element] = await loadHtml(`${base}${element}.html`);  
+  }
+
+  // find main
+  const mainArea = document.getElementById("mainArea");
+  let combinedNodes = '';
+
+  // create main content
+  for ( i = 0; i < 4; i++ ) {
+    
+    // get all relevant nodes for all
+    let key = elements[i];
+    combinedNodes = combinedNodes + html[key];
+  }
+
+  mainArea.innerHTML = combinedNodes;
+  
+}
+
+// inner html 
+const createHTML = async (file) => {
+
+  loadHtml(file)
+    .then( html => {
+      console.log(`Loaded: ${file}`)
+      mainArea.innerHTML = html 
+    })
+    .catch(console.error);
+}
+
+/*
+
+const disclaimer = document.getElementById("disclaimer");
+const privacyPolicy = document.getElementById("privacyPolicy");
+const logo = document.getElementById("logo");
+
+disclaimer.addEventListener('click', () => createHTML('impressum.html'), { once: true });
+privacyPolicy.addEventListener('click', () => createHTML('datenschutz.html'), { once: true });
+logo.addEventListener('click', (e) => console.log(e));
+*/
+
+intialize();
