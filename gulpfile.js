dc9f2e7c52a85f3d3d9d69c916af24b402491fe7
Schulz, Rene <Rene.Schulz@otto.de>
2021-12-17 16:56:03 +0100

    Restructured the folders and added gulp logic

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,14 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const sass = require('gulp-sass')(require('sass'));
 
-gulp.task('sass', () => {
-  return gulp.src('./scss/*.scss')
-  .pipe(concat('custom.scss'))
-  .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./css/'));
-});
+// scss build
+const cssBuild = () => {
+  return gulp.src('scss/*.scss')
+    .pipe(concat('style.scss'))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./'));
+}
 
-const defaultTask = (cb) => {
-  cb();
-};
-
-exports.default = defaultTask;
+exports.default = () => {
+  gulp.watch('scss/*.scss', cssBuild);
+}
